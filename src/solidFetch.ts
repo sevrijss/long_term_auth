ce5acfa6c8f82fa0fb4daedd8a99f083eab5a9a4
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2022-08-25 10:23:46 +0200

    inrupt fully works + saves credentials (in base64) *the saving mechanism is NOT secure*

--- src/solidFetch.ts
+++ src/solidFetch.ts
@@ -2,32 +2,33 @@ import rdfParser from "rdf-parse"
 import {Logger} from "./util/logger";
 import {Quad} from "rdf-js";
 import arrayifyStream from "arrayify-stream";
-import rdfDereferencer from "rdf-dereference";
 import {ask} from "./util/IoUtil";
-import {buildAuthenticatedFetch, createDpopHeader, generateDpopKeyPair, KeyPair} from '@inrupt/solid-client-authn-core';
+import {createDpopHeader, generateDpopKeyPair, KeyPair} from '@inrupt/solid-client-authn-core';
 import {Session} from "@inrupt/solid-client-authn-node";
 import {CONTENT_TYPE} from "./util/HTTPHeaders";
 import {Readable} from "stream";
+import {writeFileSync} from "fs";
+import {B64, fromB64} from "./util/StringUtils";
+
+
+const {readFileSync} = require('fs');
 
 const nodeFetch = require('node-fetch')
 
 const fetch = nodeFetch
 
+
+const STORAGE = process.cwd() + "\\config\\data.json"
+console.log(STORAGE)
+
 type INRUPT_T = "https://broker.pod.inrupt.com/"
 export const INRUPT = "https://broker.pod.inrupt.com/"
 export type supportedBrowsers = INRUPT_T
 
-type CacheRecord = {
+
+type inruptCacheRecord = {
     id: string,
     secret: string
-    /* client_id: string,
-    client_secret: string,
-    token_endpoint: string,
-    accessToken: string,
-    InruptFetch: undefined | ((input: (RequestInfo | URL), init?: RequestInit) => Promise<Response>),
-    CSSFetch: undefined | ((input: (RequestInfo | URL), init?: RequestInit) => Promise<Response>),
-
-     */
 }
 
 export default class SolidFetch {
@@ -35,85 +36,84 @@ export default class SolidFetch {
     private webID: string;
     private provider: string;
 
-    private cache: Record<string, CacheRecord> = {};
+    private inruptCache: Record<string, inruptCacheRecord>;
 
     private dpopKey: KeyPair
 
     private cachedWebID;
 
     constructor() {
+        this.loadCacheIfAvailable()
     }
 
-    async init(webID: string, provider: supportedBrowsers) {
-        this.webID = webID;
-        this.cachedWebID = webID;
-        this.dpopKey = await generateDpopKeyPair();
-        this.provider = provider;
-        if (provider === INRUPT) {
-            this.logger.info("please register this app on https://broker.pod.inrupt.com/registration.html")
-            const client_id = await ask("client id:")
-            const client_secret = await ask("client secret:")
-
-            /**
-             * code greatly derived from {@link https://communitysolidserver.github.io/CommunitySolidServer/4.x/client-credentials/ CSS}
-             */
-            const data = await (await nodeFetch(provider + "/.well-known/openid-configuration")).json()
-            let tokenUrl = data.token_endpoint;
-
-            const authString = `${encodeURIComponent(client_id)}:${encodeURIComponent(client_secret)}`;
-            const response = await nodeFetch(tokenUrl, {
-                method: 'POST',
-                headers: {
-                    // The header needs to be in base64 encoding.
-                    authorization: `Basic ${Buffer.from(authString).toString('base64')}`,
-                    'content-type': 'application/x-www-form-urlencoded',
-                    dpop: await createDpopHeader(tokenUrl, 'POST', this.dpopKey),
-                },
-                body: `grant_type=client_credentials&scope=webid openid`,
-            });
-            if (!response.ok)
-                throw new Error("Registration Failed");
-
-            const result = (await response.json())
-            const access_token = result['access_token']
-            console.log(access_token);
-
-            const authFetch = await buildAuthenticatedFetch(fetch, access_token, {dpopKey: this.dpopKey});
-
-            /* this.cache[webID] = {
-                 client_id,
-                 client_secret,
-                 token_endpoint: tokenUrl,
-                 accessToken: access_token,
-                 InruptFetch: authFetch,
-                 CSSFetch: null
-             }*/
-
+    private loadCacheIfAvailable() {
+        const data: Buffer = readFileSync(STORAGE);
+        if (!this.inruptCache) {
+            this.inruptCache = {};
+        }
+        if (data.length > 0) {
+            const base = JSON.parse(data.toString())
+            Object.keys(base).forEach((key: string) => {
+                let {id, secret} = base[key]
+                this.inruptCache[fromB64(key)] = {id: fromB64(id), secret: fromB64(secret)}
+            })
+        }
+        if (!this.inruptCache) {
+            this.inruptCache = {};
+            this.backupCache()
         }
     }
 
-    private readonly matcher = new RegExp("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?");
+    private backupCache() {
+        const base = {}
+        Object.keys(this.inruptCache).forEach((key: string) => {
+            let {id, secret} = this.inruptCache[key]
+            base[B64(key)] = {id: B64(id), secret: B64(secret)}
+        })
+        console.log(base);
+        writeFileSync(STORAGE, JSON.stringify(base))
+    }
 
     private inruptSession: Session;
-    private inruptWebID;
 
-    async fetch(TYPE: "CSS" | "INRUPT", provider: string, url: string, webID: string): Promise<Quad[]> {
+    async fetch(provider: string, url: string, webID: string): Promise<Quad[]> {
         if (!provider.endsWith("/")) {
             provider += "/"
         }
-        const result: Response = await nodeFetch(url)
-        if (result.ok) {
-            const data: Quad[] = await arrayifyStream((await rdfDereferencer.dereference(url)).data)
-            console.log(data);
-            return data;
+        let result: Response
+        let failed = false
+        try {
+            console.log("trying normal fetch");
+            result = await fetch(url)
+            if (!result.ok) {
+                throw new Error("failed");
+            }
+        } catch (e) {
+            console.log(e);
+            failed = true;
+        }
+        if (!failed) {
+            const data = await result.text();
+            const contentType = result.headers.get(CONTENT_TYPE).split(";")[0]
+            return await arrayifyStream(rdfParser.parse(Readable.from([data]), {
+                contentType: contentType,
+                baseIRI: url,
+            }))
         } else {
-            this.logger.warn(`fetch failed with code: ${result.status} ${result.statusText}`)
-            this.logger.info("trying an authorized approach");
+            this.logger.warn("fetch failed, trying an authorized approach")
 
             if (provider === INRUPT) {
                 this.logger.info("please register this app on https://broker.pod.inrupt.com/registration.html")
-                const client_id = await ask("client id:")
-                const client_secret = await ask("client secret:")
+                let client_secret, client_id;
+                if (!this.inruptCache[webID]) {
+                    client_id = await ask("client id:")
+                    client_secret = await ask("client secret:")
+                    this.inruptCache[webID] = {id: client_id, secret: client_secret}
+                    this.backupCache()
+                } else {
+                    client_id = this.inruptCache[webID].id
+                    client_secret = this.inruptCache[webID].secret
+                }
                 if (!this.webID || this.webID !== webID) {
                     this.inruptSession = new Session();
                     this.webID = webID;
@@ -130,18 +130,14 @@ export default class SolidFetch {
 
                     if (this.inruptSession.info.isLoggedIn) {
                         console.info("INFO::::::::: Logged In with Client Credentials.");
-                        console.log(this.inruptSession.info);
                         // Perform some operation
                         res = await this.inruptSession.fetch(url);
                         const data = await res.text();
                         const contentType = res.headers.get(CONTENT_TYPE).split(";")[0]
-                        console.log(contentType)
-                        console.log(data);
                         const quads: Quad[] = await arrayifyStream(rdfParser.parse(Readable.from([data]), {
                             contentType: contentType,
                             baseIRI: url,
                         }))
-                        console.log(quads);
                         return quads
                     }
                 } catch (err) {
@@ -190,70 +186,67 @@ export default class SolidFetch {
                      InruptFetch: authFetch,
                      CSSFetch: null
                  }*/
-                return;
-
             }
 
-            if (TYPE === "CSS") {
-                let id, secret;
-                if (this.cache[webID]) {
-                    id = this.cache[webID].id;
-                    secret = this.cache[webID].secret
-                } else {
-                    const email = await ask("email");
-                    const passwd = await ask("password:", true);
+            let id, secret;
+            if (false) {//this.cache[webID]) {
+                // id = this.cache[webID].id;
+                // secret = this.cache[webID].secret
+            } else {
+                const email = await ask("email");
+                const passwd = await ask("password:", true);
 
-                    const base = "http://localhost:3000"
+                const base = "http://localhost:3000"
 
-                    // This assumes your server is started under http://localhost:3000/.
-                    // This URL can also be found by checking the controls in JSON responses when interacting with the IDP API,
-                    // as described in the Identity Provider section.
-                    const response = await fetch(`${base}/idp/credentials/`, {
-                        method: 'POST',
-                        headers: {'content-type': 'application/json'},
-                        // The email/password fields are those of your account.
-                        // The name field will be used when generating the ID of your token.
-                        body: JSON.stringify({
-                            email: email,
-                            password: passwd,
-                            name: `auth_fetch_${Date.now().toString()}`
-                        }),
-                    });
-
-                    // These are the identifier and secret of your token.
-                    // Store the secret somewhere safe as there is no way to request it again from the server!
-                    const json = await response.json();
-                    console.log(json);
-                    id = json.id;
-                    secret = json.secret;
-                }
-                const dpopKey = await generateDpopKeyPair();
-                const tokenUrl = "http://localhost:3000"
-                // These are the ID and secret generated in the previous step.
-                // Both the ID and the secret need to be form-encoded.
-                const authString = `${encodeURIComponent(id)}:${encodeURIComponent(secret)}`;
-                // This URL can be found by looking at the "token_endpoint" field at
-                // http://localhost:3000/.well-known/openid-configuration
-                // if your server is hosted at http://localhost:3000/.
-                const response = await fetch(tokenUrl, {
+                // This assumes your server is started under http://localhost:3000/.
+                // This URL can also be found by checking the controls in JSON responses when interacting with the IDP API,
+                // as described in the Identity Provider section.
+                const response = await fetch(`${base}/idp/credentials/`, {
                     method: 'POST',
-                    headers: {
-                        // The header needs to be in base64 encoding.
-                        authorization: `Basic ${Buffer.from(authString).toString('base64')}`,
-                        'content-type': 'application/x-www-form-urlencoded',
-                        dpop: await createDpopHeader(tokenUrl, 'POST', dpopKey),
-                    },
-                    body: 'grant_type=client_credentials&scope=webid',
+                    headers: {'content-type': 'application/json'},
+                    // The email/password fields are those of your account.
+                    // The name field will be used when generating the ID of your token.
+                    body: JSON.stringify({
+                        email: email,
+                        password: passwd,
+                        name: `auth_fetch_${Date.now().toString()}`
+                    }),
                 });
 
-                // This is the Access token that will be used to do an authenticated request to the server.
-                // The JSON also contains an "expires_in" field in seconds,
-                // which you can use to know when you need request a new Access token.
-                const {access_token: accessToken} = await response.json();
-                console.log(accessToken);
+                // These are the identifier and secret of your token.
+                // Store the secret somewhere safe as there is no way to request it again from the server!
+                const json = await response.json();
+                console.log(json);
+                id = json.id;
+                secret = json.secret;
             }
+            const dpopKey = await generateDpopKeyPair();
+            const tokenUrl = "http://localhost:3000"
+            // These are the ID and secret generated in the previous step.
+            // Both the ID and the secret need to be form-encoded.
+            const authString = `${encodeURIComponent(id)}:${encodeURIComponent(secret)}`;
+            // This URL can be found by looking at the "token_endpoint" field at
+            // http://localhost:3000/.well-known/openid-configuration
+            // if your server is hosted at http://localhost:3000/.
+            const response = await fetch(tokenUrl, {
+                method: 'POST',
+                headers: {
+                    // The header needs to be in base64 encoding.
+                    authorization: `Basic ${Buffer.from(authString).toString('base64')}`,
+                    'content-type': 'application/x-www-form-urlencoded',
+                    dpop: await createDpopHeader(tokenUrl, 'POST', dpopKey),
+                },
+                body: 'grant_type=client_credentials&scope=webid',
+            });
 
+            // This is the Access token that will be used to do an authenticated request to the server.
+            // The JSON also contains an "expires_in" field in seconds,
+            // which you can use to know when you need request a new Access token.
+            const {access_token: accessToken} = await response.json();
+            console.log(accessToken);
         }
+
     }
+
 
 }
